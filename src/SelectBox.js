b6415cbbf4833e3e9d45c6f994e41b8a7518fe35
Barbara Post <post.barbara@gmail.com>
2015-09-30 13:29:05 +0200

    Work in progress on integrating "how to use more than one select box" PR.

--- src/SelectBox.js
+++ src/SelectBox.js
@@ -13,14 +13,28 @@ angular.module('$selectBox', []).directive('selectBox', function () {
         restrict: 'E',
         require: ['ngModel' ],
         template: _template,
-        controller: function ($scope, $element, $attrs, $ionicModal, $parse) {
+        scope: {
+            ngSelectedValue: "@",
+            ngTitle: "@",
+            ngItemName: "@",
+            ngItemId: "@",
+            ngData: "@",
+            ngPlaceholder: "@",
+            ngHeaderClass: "@",
+            ngSelectChanged: "@"
+        },
+        controller: function ($scope, $element, $scope, $ionicModal, $parse) {
 
-            $scope.label = ($attrs.ngPlaceholder) ? $attrs.ngPlaceholder : "";
+            $scope.label = ($scope.ngPlaceholder) ? $scope.ngPlaceholder : "";
 
             $scope.modal = {};
 
             $scope.showSelectModal = function () {
-                var val = $parse($attrs.ngData);
+                // TODO keep debugging, doesn't work out of the box
+                // TODO also update test page with a second select item.
+                console.log($scope.ngData);
+                console.log($scope.data);
+                var val = $parse($scope.ngData);
                 $scope.data = val($scope);
 
                 $scope.modal.show();
@@ -35,13 +49,13 @@ angular.module('$selectBox', []).directive('selectBox', function () {
             });
 
             $scope.modal = $ionicModal.fromTemplate('<ion-modal-view id="select">'
-                + '<ion-header-bar '+(($attrs.ngHeaderClass) ? 'class="'+$attrs.ngHeaderClass+'"' : '') +'>'
-                + '<h1 class="title">' + $attrs.ngTitle + '</h1>'
+                + '<ion-header-bar '+(($scope.ngHeaderClass) ? 'class="'+$scope.ngHeaderClass+'"' : '') +'>'
+                + '<h1 class="title">' + $scope.ngTitle + '</h1>'
                 + ' <a ng-click="closeSelectModal()" class="button button-icon icon ion-close"></a>'
                 + '</ion-header-bar>'
                 + '<ion-content>'
                 + '<ion-list>'
-                + '<ion-item  ng-click="clickItem(item);' + $attrs.ngSelectChanged + '" ng-repeat="item in data" ng-bind-html="item[\'' + $attrs.ngItemName + '\']"></ion-item>'
+                + '<ion-item  ng-click="clickItem(item);' + $scope.ngSelectChanged + '" ng-repeat="item in data" ng-bind-html="item[\'' + $scope.ngItemName + '\']"></ion-item>'
                 + '</ion-list>'
                 + ' </ion-content>'
                 + '</ion-modal-view>', {
@@ -51,22 +65,22 @@ angular.module('$selectBox', []).directive('selectBox', function () {
 
             $scope.clickItem = function (item) {
 
-                var value = $parse($attrs.ngSelectedValue);
-                value.assign($scope.$parent, item[$attrs.ngItemId]);
+                var value = $parse($scope.ngSelectedValue);
+                value.assign($scope.$parent, item[$scope.ngItemId]);
 
-                $scope.label = item[$attrs.ngItemName];
+                $scope.label = item[$scope.ngItemName];
                 $scope.closeSelectModal();
             };
 
             $scope.$on('reset', function(){
-                $scope.label =  ($attrs.ngPlaceholder) ? $attrs.ngPlaceholder : "";
+                $scope.label =  ($scope.ngPlaceholder) ? $scope.ngPlaceholder : "";
             })
         },
-        compile: function ($element, $attrs) {
+        compile: function ($element, $scope) {
             var input = $element.find('input.selected');
             angular.forEach({
-                'name': $attrs.name,
-                'ng-model': $attrs.ngSelectedValue
+                'name': $scope.name,
+                'ng-model': $scope.ngSelectedValue
             }, function (value, name) {
                 if (angular.isDefined(value)) {
                     input.attr(name, value);
